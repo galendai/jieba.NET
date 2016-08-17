56ef9aee0e123265cc18fccda9ba5e8ce5ef8576
Galen Dai <galen@leanwing.com>
2016-08-18 00:08:34 +0800

    porting Segmenter.cs and Analyser.cs

---
+++ src/Analyser.core/ConfigManager.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace JiebaNet.Analyser
+{
+    public class ConfigManager
+    {
+        // TODO: duplicate codes.
+        public static string ConfigFileBaseDir
+        {
+            get
+            {
+                var config = new ConfigurationBuilder()
+                                .AddXmlFile("app.config")
+                                .Build();
+                return config["JiebaConfigFileDir"] ?? "Resources";
+                //return ConfigurationManager.AppSettings["JiebaConfigFileDir"] ?? "Resources";
+            }
+        }
+
+        public static string IdfFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "idf.txt"); }
+        }
+
+        public static string StopWordsFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "stopwords.txt"); }
+        }
+    }
+}
---
+++ src/Segmenter.core/ConfigManager.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace JiebaNet.Segmenter
+{
+    public class ConfigManager
+    {
+        public static string ConfigFileBaseDir
+        {
+            get
+            {
+                var config = new ConfigurationBuilder()
+                                .AddXmlFile("app.config")
+                                .Build();
+                return config["JiebaConfigFileDir"] ?? "Resource";
+                //return ConfigurationManager.AppSettings["JiebaConfigFileDir"] ?? "Resources";
+            }
+        }
+
+        public static string MainDictFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "dict.txt"); }
+        }
+
+        public static string ProbTransFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "prob_trans.json"); }
+        }
+
+        public static string ProbEmitFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "prob_emit.json"); }
+        }
+
+        public static string PosProbStartFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "pos_prob_start.json"); }
+        }
+
+        public static string PosProbTransFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "pos_prob_trans.json"); }
+        }
+
+        public static string PosProbEmitFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "pos_prob_emit.json"); }
+        }
+
+        public static string CharStateTabFile
+        {
+            get { return Path.Combine(ConfigFileBaseDir, "char_state_tab.json"); }
+        }
+    }
+}
